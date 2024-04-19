d59ade7e10dd9a626b15a8dc0bad4d0c50430899
Antoine Dupont <tony_dupont@yahoo.com>
2024-04-19 17:05:10 +0200

    storing the datas

--- Myfunctions.py
+++ Myfunctions.py
@@ -5,3 +5,15 @@ def display_table_from_file(file_name):
         table = [line.strip().split() for line in lines]
         for row in table:
             print("\t".join(row))
+
+def create_table_dict_from_file(file_name):
+    table_dict = {}
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            parts = line.strip().split()
+            edge_name = int(parts[0])
+            duration = int(parts[1])
+            predecessors = list(map(int, parts[2:]))
+            table_dict[edge_name] = {"duration": duration, "predecessors": predecessors}
+    return table_dict
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import Myfunctions
 
 def main():
     # Exemple d'utilisation :
-    file_name = "table_1.txt"  # Remplacez "exemple.txt" par le nom de votre fichier
+    file_name = "table 1.txt"  # Remplacez "exemple.txt" par le nom de votre fichier
     Myfunctions.display_table_from_file(file_name)
+    table_dict = Myfunctions.create_table_dict_from_file(file_name)
+    print(table_dict)
     
 if __name__ == "__main__":
     main()
